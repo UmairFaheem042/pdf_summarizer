7b66df696407533830e1b886397dfe82a8ea36b8
UmairFaheem042 <umair.freelancer042@gmail.com>
2025-03-15 22:50:26 +0530

    initial setup done

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-2xl">Home</h1>
+      <Button variant="secondary" size="lg">Hello World</Button>
+    </div>
+  );
 };
 
 export default Home;
